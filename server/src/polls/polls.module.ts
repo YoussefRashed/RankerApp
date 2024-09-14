6cc8a99c83fe0c7378391ca188dc4ebee04b81e0
YoussefRashed <YousefRashed.Business@gmail.com>
2024-09-14 15:37:13 +0300

    adding Auth Guard

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { redisModule } from 'src/modules.config';
+import { redisModule, jwtModule } from 'src/modules.config';
 import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 import { PollsController } from './polls.controller';
 
 @Module({
-  imports: [ConfigModule, redisModule],
+  imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
